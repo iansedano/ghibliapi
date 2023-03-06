ea81ca636d622d2af2c21a76dcf796dc38e3eb6b
Ian Currie <iansedano@gmail.com>
2023-03-06 17:20:33 +0100

    removing filterFields

--- api/[type]/[query].js
+++ api/[type]/[query].js
@@ -1,26 +1,16 @@
-import data from "../../data.json"
+import data from "../../data.json";
 
 export const config = {
-	runtime: 'edge',
+  runtime: "edge",
 };
-
-filterFields = (obj, fields) => {
-  Object.keys(obj).forEach((prop) => {
-    if (!fields.includes(prop)) {
-      delete obj[prop]
-    }
-  })
-  return obj
-}
 
 export default (req) => {
-	const params = new URL(req.url).searchParams
-        const type = params.get('type');
-	const query = params.get('query');
-	const out = {};
-	out[type] = data[type].filter(item => item.id == query);
-	return Response.json({
-		data: data[type].filter(item => item.id == query)
-	});
+  const params = new URL(req.url).searchParams;
+  const type = params.get("type");
+  const query = params.get("query");
+  const out = {};
+  out[type] = data[type].filter((item) => item.id == query);
+  return Response.json({
+    data: data[type].filter((item) => item.id == query),
+  });
 };
-
